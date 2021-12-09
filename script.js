0fb3a695fb94ca71d954c48d3b3c1abcbf4d8694
ouarab.khadidja <ouarab.khadidja@gmail.com>
2021-12-09 10:50:39 +0100

    overllaping same value

--- script.js
+++ script.js
@@ -35,3 +35,39 @@ function overlappingFunction(set1, set2) {
 console.log(overlappingFunction(set1, set2));
 
 
+//******************************************* */
+
+function overlappingFunction1(set1, set2) {
+
+    let set = []
+    let sum = 0
+    let trouv = 0
+    for (let i = 0; i < set1.length; i++) {
+        for (let j = 0; j < set2.length; j++) {
+            if (set1[i] == set2[j]) {
+                trouv = 1
+            }
+        }
+        if (trouv == 1) {
+            sum = sum + set1[i]
+        }
+        trouv = 0
+    }
+
+    for (let i = 0; i < set2.length; i++) {
+        for (let j = 0; j < set1.length; j++) {
+            if (set1[j] == set2[i]) {
+                trouv = 1
+            }
+        }
+        if (trouv == 1) {
+            sum = sum + set2[i]
+        }
+        trouv = 0
+    }
+
+    return sum
+}
+console.log(overlappingFunction1(set1, set2));
+
+
